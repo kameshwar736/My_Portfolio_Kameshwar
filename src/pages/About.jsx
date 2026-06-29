12a30ceeca0a27d56bd10e115d2e92b5a4f6579e
Kameshwar <kameshwart736@gmail.com>
2026-06-29 12:09:46 +0530

    tech stack updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import html from '../assets/HTML5.png'
+import css from '../assets/CSS3.png'
+import bootstrap from '../assets/Bootstrap.png'
+import tailwind from '../assets/Tailwind CSS.png'
+import js from '../assets/JavaScript.png'
+import react from '../assets/React.png'
+import nodejs from '../assets/Node.js.png'
+import express from '../assets/expressWhite.png'
+import mongodb from '../assets/MongoDB.png'
+import mysql from '../assets/MySQL.png'
+import git from '../assets/Git.png'
+import github from '../assets/gitLogo.png'
+
 
 const About = () => {
+
+  const img =  [html, css, js, bootstrap, tailwind, react, nodejs, express, mongodb, mysql, git, github]
+
+  const slideImages = [...img,...img,...img]
+  
+  
+
+
+
+
   return (
     <>
-      <hr style={{ color: 'white' }} />
-      <div className='pl-15 pt-10 bg-[rgb(18_18_18)] text-gray-300 h-screen flex flex-col'>
+     
+      <div className='pl-15 pt-10 bg-[rgb(18_18_18)] text-gray-300 h-screen flex flex-col gap-5'>
 
-        <div className='flex p-10 gap-10'>
+        <div className='flex px-10 pt-10 gap-10'>
 
-          <div className='w-200 flex flex-col gap-3 border-r-2 '>
+          <div className='w-200 flex flex-col gap-3 border-r-2 border-[rgb(239,135,55)] '>
             <div>
               <div style={{ fontFamily: "sans-serif" }} className=' text-[rgb(239,135,55)] text-4xl '>
-              About Me
-            </div>
-            <div className=' '>
-              <p className=' text-xl text-gray-400 p-5 pr-10'>
-                Hi, I’m <strong>Kameshwar</strong>, a Frontend Developer skilled in <strong>React, JavaScript, and Tailwind CSS</strong>. I build responsive, user-friendly web applications and have working knowledge of <strong>Node.js, Express, and Git</strong>. Currently seeking an opportunity to contribute and grow as a developer.
-              </p>
+                About Me
+              </div>
+              <div className=' '>
+                <p className=' text-xl text-gray-400 p-5 pr-10'>
+                  Hi, I’m <strong>Kameshwar</strong>, a Frontend Developer skilled in <strong>React, JavaScript, and Tailwind CSS</strong>. I build responsive, user-friendly web applications and have working knowledge of <strong>Node.js, Express, and Git</strong>. Currently seeking an opportunity to contribute and grow as a developer.
+                </p>
 
-            </div>
+              </div>
             </div>
 
             <div >
-              <div style={{ fontFamily: "sans-serif" }} className=' text-[rgb(239,135,55)] text-4xl '>
+              <div style={{ fontFamily: "sans-serif" }} className=' text-[rgb(239,135,55)] text-3xl '>
                 Education
               </div>
               <div className='flex flex-col gap-2 p-5'>
@@ -40,7 +63,7 @@ const About = () => {
 
 
           <div className='flex flex-col gap-1 '>
-            <div style={{ fontFamily: "sans-serif" }} className=' text-[rgb(239,135,55)]  flex flex-col gap-3 text-4xl '>
+            <div style={{ fontFamily: "sans-serif" }} className=' text-[rgb(239,135,55)]  flex flex-col gap-3 text-3xl '>
               InternShip
             </div>
 
@@ -68,22 +91,30 @@ const About = () => {
 
         </div>
 
-        <div>
-          <div className='text-4xl font-sans text-[rgb(239,135,55)] p-10 mask-b-from-neutral-900'>
+         <div>
+          <div className='text-4xl font-sans text-[rgb(239,135,55)] p-10'>
             Tech Stack
           </div>
-          <div>
-
-            courousel
-
-
+          <div className="relative overflow-hidden mx-10 ">
+            <div
+              style={{ animation: 'scroll 30s linear infinite' }}
+              className="flex gap-20"
+            >
+              {slideImages.map((img, i) => (
+                <img
+                  key={i}
+                  src={img}
+                  className="w-25 flex-shrink-0 rounded-lg object-contain"
+                />
+              ))}
+            </div>
           </div>
         </div>
-       
-          
-      </div>  
 
-     
+
+      </div>
+
+
 
     </>
   )
